9122289f68bf5342a0bef52f45abc2be3167ac3e
Romain Franconville <franconviller@janelia.hhmi.org>
2012-04-09 16:42:47 -0400

    Packaging of R utility functions for Ovation

---
+++ R/RovationTools.R
@@ -0,0 +1,83 @@
+newDataContext <- function
+### Returns a DataContext object after prompting for password
+  (connection,
+   ### Connection to the database
+   login
+   ### User login
+   ){
+  require(tcltk)  # The prompt uses the tcltk library
+  tt<-tktoplevel()
+  tkwm.geometry(tt,"+700+400")
+  tktitle(tt) <- "Log in OvationDB"
+  Password <- tclVar("") 
+  entry.Password <-tkentry(tt,width="20",textvariable=Password,show="*") 
+  tkgrid(tklabel(tt,text="Please enter your password.")) 
+  tkgrid(entry.Password)
+  OnOK <- function() 
+  { 
+     tkdestroy(tt)	
+     Password <<- tclvalue(Password)     
+  } 
+  OK.but <-tkbutton(tt,text="   OK   ",command=OnOK)
+  tkfocus(entry.Password)
+  tkbind(entry.Password, "<Return>",OnOK) 
+  tkgrid(OK.but) 
+  tkwait.window(tt)
+  Ovation <- .jnew("ovation/Ovation")
+  dataContext <- Ovation$connect(connection,login,Password)
+  return(dataContext)
+  ### A reference to a DataContext object
+}
+
+
+datetime <- function
+### Construct an Ovation timestamp from date components.
+(year,month,day,hour=0,minute=0,second=0,millisecond=0,timezone=character())
+  {
+     if (length(timezone)==0){
+       timezone <- .jcall("org/joda/time/DateTimeZone","Lorg/joda/time/DateTimeZone;",method="getDefault")
+       }else{
+         timezone <- .jcall("org/joda/time/DateTimeZone","Lorg/joda/time/DateTimeZone;",method="forID",timezone)}
+       
+    .jnew("org/joda/time/DateTime",as.integer(year),as.integer(month),as.integer(day),as.integer(hour),as.integer(minute),as.integer(second),as.integer(millisecond),timezone)
+     ### A DateTime object
+        }
+
+nextJavaIt <- function
+### Wrapper for applying the "next" method to an iterator, because "next" is a protected word in R
+(iterator){
+    .jrcall(iterator,"next")
+   ### Returns the next object the iterator is refering to 
+}
+
+list2map <- function
+### Converts an R list to a Java map
+(l
+ ### A list
+ ){
+  keys <- names(l)
+  m <- .jnew("java/util/HashMap",length(l))
+  lapply(keys,function(k){
+    value <- l[[k]]
+## The put method requires wrapper classes input, so we need to convert beforehand (character is automatically converted to String by R, and vectors to java arrays).
+    if(is.logical(value)){
+      value <- .jnew("java/lang/Boolean",value)
+    }
+    
+    if(is.raw(value)){
+      value <- .jbyte(value)
+    }
+    
+    if(is.double(value) & length(value)==1){
+      value <- .jnew("java/lang/Float",value)
+    }
+    
+    if(is.integer(value) & length(value)==1){
+      value <- .jnew("java/lang/Integer",value)
+    }
+    m$put(k,value)
+  })
+
+  return(m)
+  ### A Java map         
+       }
---
+++ R/ovationRStartup.R
@@ -0,0 +1,26 @@
+.onLoad <- function(libname, pkgname) {
+  if (Sys.info()["sysname"]=="Linux"){
+    Sys.setenv(OBJY_ROOT="/usr/object/linux86_64")
+    Sys.setenv(OVATION_ROOT="/usr/ovation")
+    libraryPaths <- c("/usr/ovation/lib", "/usr/object/linux86_64/lib")
+  }else if (Sys.info()["sysname"]=="Windows"){
+    libraryPaths <- c(file.path(Sys.getenv("OVATION_ROOT"),"bin"),
+                      file.path(Sys.getenv("OBJY_ROOT"),"lib"),
+                      file.path(Sys.getenv("OBJY_ROOT"),"bin")) 
+    
+  }else if (Sys.info()["sysname"]=="Darwin"){
+    Sys.setenv(OBJY_ROOT="/opt/object/mac86_64")
+    Sys.setenv(OVATION_ROOT="/opt/ovation")
+    libraryPaths <- c("/opt/ovation/lib", "/opt/object/mac86_64/lib")
+    
+  }else stop("Platform not supported")
+
+  ovationJarPath <-file.path(Sys.getenv("OVATION_ROOT"),"ovation.jar")
+  
+  if (Sys.info()["sysname"]=="Windows"){
+    ooJarPath <-file.path(Sys.getenv("OBJY_ROOT"), "lib", "oojava.jar") 
+  }else {ooJarPath <-file.path(Sys.getenv("OBJY_ROOT"),"java", "lib", "oojava.jar") 
+  }
+  
+  .jpackage(pkgname, lib.loc=libname,morePaths=c(ovationJarPath,ooJarPath))
+}
