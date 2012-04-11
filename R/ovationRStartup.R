7b3e9a760aeba0c750f9323b31bf0a4ef25256f3
Barry Wark <barrywark@gmail.com>
2012-04-11 15:45:26 -0400

    minore format cleanup

--- R/ovationRStartup.R
+++ R/ovationRStartup.R
@@ -6,23 +6,24 @@
     Sys.setenv(OBJY_ROOT="/usr/object/linux86_64")
     Sys.setenv(OVATION_ROOT="/usr/ovation")
     libraryPaths <- c("/usr/ovation/lib", "/usr/object/linux86_64/lib")
-  }else if (Sys.info()["sysname"]=="Windows"){
+  } else if (Sys.info()["sysname"]=="Windows"){
     libraryPaths <- c(file.path(Sys.getenv("OVATION_ROOT"),"bin"),
                       file.path(Sys.getenv("OBJY_ROOT"),"lib"),
                       file.path(Sys.getenv("OBJY_ROOT"),"bin")) 
     
-  }else if (Sys.info()["sysname"]=="Darwin"){
+  } else if (Sys.info()["sysname"]=="Darwin"){
     Sys.setenv(OBJY_ROOT="/opt/object/mac86_64")
     Sys.setenv(OVATION_ROOT="/opt/ovation")
     libraryPaths <- c("/opt/ovation/lib", "/opt/object/mac86_64/lib")
     
-  }else stop("Platform not supported")
+  } else stop("Platform not supported")
 
   ovationJarPath <-file.path(Sys.getenv("OVATION_ROOT"),"ovation.jar")
   
   if (Sys.info()["sysname"]=="Windows"){
     ooJarPath <-file.path(Sys.getenv("OBJY_ROOT"), "lib", "oojava.jar") 
-  }else {ooJarPath <-file.path(Sys.getenv("OBJY_ROOT"),"java", "lib", "oojava.jar") 
+  } else {
+	  ooJarPath <-file.path(Sys.getenv("OBJY_ROOT"),"java", "lib", "oojava.jar") 
   }
   
   .jpackage(pkgname, lib.loc=libname,morePaths=c(ovationJarPath,ooJarPath))
